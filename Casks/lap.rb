79feafd3624160c3ec3e7b2d329f32625dfb88c1
julyXX <liulichuan@live.cn>
2026-05-12 11:40:17 +0800

    Update download url

--- Casks/lap.rb
+++ Casks/lap.rb
@@ -5,7 +5,7 @@ cask "lap" do
   sha256 arm:   "c5f91626971fe7067f4bbaec68c6d422957380a129576928bc9b6cc0d4d7e82b",
          intel: "d603ac556fec1e67ed2b849dcc8d19181813688ace7620801f8d8db53b0bb882"
 
-  url "https://github.com/julyx10/lap/releases/tag/v#{version}/Lap_#{version}_#{arch}.dmg",
+  url "https://github.com/julyx10/lap/releases/download/v#{version}/Lap_#{version}_#{arch}.dmg",
       verified: "github.com/julyx10/lap/"
   name "Lap"
   desc "Private local photo manager"
